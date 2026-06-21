38c8d4759f70164b0656d0dd52ec3a2b7eec41db
Harshit Sharma <harshitsharma200377@gmail.com>
2026-06-22 01:44:31 +0530

    Update vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,4 +1,4 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from embeddings import get_embeddings
 
 def create_vectorstore(chunks):
@@ -13,12 +13,3 @@ def load_vectorstore():
         persist_directory="chroma_db",
         embedding_function=get_embeddings()
     )
-
-if __name__ == "__main__":
-    from loader import load_documents
-    from splitter import split_documents
-    docs = load_documents()
-    chunks = split_documents(docs)
-    db = create_vectorstore(chunks)
-    print(f"✅ Vectorstore created!")
-    print(f"Total vectors saved: {db._collection.count()}")
